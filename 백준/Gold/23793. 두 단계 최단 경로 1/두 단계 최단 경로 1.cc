bbbd21ae3f4c60b1c719ebd9f3606300cedf1dc0
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-03-25 03:00:08 +0900

    [Gold IV] Title: 두 단계 최단 경로 1, Time: 120 ms, Memory: 10388 KB -BaekjoonHub

--- 백준/Gold/23793. 두 단계 최단 경로 1/두 단계 최단 경로 1.cc
+++ 백준/Gold/23793. 두 단계 최단 경로 1/두 단계 최단 경로 1.cc
@@ -1,119 +1,89 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <climits>
-#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
-#define MAX 100001
-#define LL long long
-#define INF 1e9
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
 
 using namespace std;
-int N, M, X, Y, Z;
-vector<pair<int, int> > Edge[MAX];
-int Cost[3][MAX];
 
-void Input() {
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		Cost[0][i] = INF;
-		Cost[1][i] = INF;
-		Cost[2][i] = INF;
-	}
-	for (int i = 0; i < M; i++) {
-		int U, V, W;
-		cin >> U >> V >> W;
-		Edge[U].push_back(make_pair(V, W));
-	}
-	cin >> X >> Y >> Z;
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 }
 
-void Dijkstra1(int I, int S, int E) { // 정점 X에서 출발해서 정점 Y를 거치는 경우
-	priority_queue<pair<int, int> > PQ;
-	Cost[I][S] = 0;
-	PQ.push(make_pair(0, S));
+#define INF 1000000000
 
-	while (!PQ.empty()) {
-		int CurCost = -PQ.top().first;
-		int CurX = PQ.top().second;
-		PQ.pop();
+vector<pair<int, int>>v[100001];
 
-		if (CurX == E) {
-			return;
-		}
 
-		if (Cost[I][CurX] < CurCost) {
+int dist1[100001];
+
+int dist2[100001];
+
+int x, y, z,check;
+
+void dijk(int s) {
+	priority_queue<pair<long long int, int>>pq;
+	pq.push({ 0,s });
+	dist1[s] = 0;
+	while (!pq.empty()) {
+		long long int d = -pq.top().first;
+		s = pq.top().second;
+		pq.pop();
+		if (dist1[s] < d)
 			continue;
-		}
-
-		for (int i = 0; i < Edge[CurX].size(); i++) {
-			int nextCost = Edge[CurX][i].second;
-			int nextX = Edge[CurX][i].first;
-			if (Cost[I][nextX] > CurCost + nextCost) {
-				Cost[I][nextX] = CurCost + nextCost;
-				PQ.push(make_pair(-Cost[I][nextX], nextX));
-			}
-		}
-	};
-}
-
-void Dijkstra2() { // 정점 X에서 출발해서 정점 Y를 거치지 않는 경우
-	priority_queue<pair<int, int> > PQ;
-	Cost[2][X] = 0;
-	PQ.push(make_pair(0, X));
-
-	while (!PQ.empty()) {
-		int CurCost = -PQ.top().first;
-		int CurX = PQ.top().second;
-		PQ.pop();
-
-		if (Cost[2][CurX] < CurCost) {
-			continue;
-		}
-
-		for (int i = 0; i < Edge[CurX].size(); i++) {
-			int nextCost = Edge[CurX][i].second;
-			int nextX = Edge[CurX][i].first;
-			if (nextX == Y) {
+		for (int i = 0; i < v[s].size(); i++) {
+			int next = v[s][i].first;
+			long long int cost = d + v[s][i].second;
+			if (check == next)
 				continue;
-			}
-			if (Cost[2][nextX] > CurCost + nextCost) {
-				Cost[2][nextX] = CurCost + nextCost;
-				PQ.push(make_pair(-Cost[2][nextX], nextX));
+			if (cost <dist1[next]) {
+				dist1[next] = cost;
+				pq.push({ -cost,next });
 			}
 		}
-	};
+	}
 }
 
-void Find_Answer() {
-	if ((Cost[0][Y] != INF) && (Cost[1][Z] != INF)) {
-		cout << Cost[0][Y] + Cost[1][Z] << " ";
-	}
-	else {
-		cout << -1 << " ";
-	}
-	if (Cost[2][Z] != INF) {
-		cout << Cost[2][Z] << " ";
-	}
-	else {
-		cout << -1 << " ";
-	}
-	cout << "\n";
-}
 
-int main() {
-	FASTIO
-	Input();
-	Dijkstra1(0, X, Y);
-	Dijkstra1(1, Y, Z);
-	Dijkstra2();
-	Find_Answer();
 
+
+
+int main(void) {
+	fastio();
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int a, b, cost;
+		cin >> a >> b >> cost;
+		v[a].push_back({ b,cost });
+	}
+
+	cin >> x >> y >> z;
+	
+	fill(dist1, dist1 + sizeof(dist1) / sizeof(int), INF);
+
+	dijk(x);
+	long long int xy = dist1[y];
+
+	fill(dist1, dist1 + sizeof(dist1) / sizeof(int), INF);
+
+	dijk(y);
+	long long int yz = dist1[z];
+
+	if (xy!=INF&&yz!=INF)
+		cout << xy+yz<< ' ';
+	else
+		cout << -1 << ' ';
+	
+	check = y;
+	fill(dist1, dist1 + sizeof(dist1) / sizeof(int), INF);
+
+	dijk(x);
+
+	if (dist1[z] != INF)
+		cout << dist1[z] << ' '<<'\n';
+	else
+		cout << -1 << ' '<<'\n';
 	return 0;
 }
-
