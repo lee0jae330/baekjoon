6030c38b092f79cb4f10fb0435747e703329aafd
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-03-26 00:19:46 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	int sum1 = a * 6 + 3 * b + 2 * c + d + 2 * e;
+	cin >> a >> b >> c >> d >> e;
+	cout << sum1 <<' ' << a * 6 + 3 * b + 2 * c + d + 2 * e << '\n';
+	return 0;
+}
