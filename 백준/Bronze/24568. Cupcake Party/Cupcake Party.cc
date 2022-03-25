d8625eca50b92b7bdbf57cf922b377ecd6acf1a7
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-03-26 00:16:59 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(void) {
+	int a, b;
+	cin >> a >> b;
+	int total = 8 * a + 3 * b;
+	cout << total - 28 << '\n';
+	return 0;
+}
