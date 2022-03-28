78bb2c7908b3185b8893b68803dd38e8d18f6a97
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-03-28 15:17:50 +0900

    [Silver II] Title: RPG 마스터 오명진, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/22941. RPG 마스터 오명진/RPG 마스터 오명진.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	long long hp1, atk1, hp2, atk2;
+	cin >> hp1 >> atk1 >> hp2 >> atk2;
+	long long P, S;
+	cin >> P >> S;
+	long long knight = hp2 / atk1;
+	if (hp2 % atk1)
+		knight++;
+	long long devil = hp1 / atk2;
+	if (hp1 % atk2)
+		devil++;
+	if (knight > devil)
+		cout << "gg" << '\n';
+	else {
+		int ishealed = 0;
+		if (hp2 % atk1 == 0) {
+			long long n = hp2 / atk1;
+			if (n > 1 && P >= atk1)
+				ishealed = 1;
+		}
+		else {
+			//long long n = hp2 / atk1;
+			if (hp2 % atk1 <= P)
+				ishealed = 1;
+		}
+		if (ishealed) {
+			long long before = hp2 - P;
+			long long num = before / atk1;
+			if (before % atk1)
+				num++;
+			long long after = hp2 - num * atk1 + S;
+			long long tmp = after / atk1;
+			if (after % atk1)
+				tmp++;
+			long long result = num + tmp;
+			if (result > devil)
+				cout << "gg" << '\n';
+			else
+				cout << "Victory!" << '\n';
+		}
+		else {
+			cout << "Victory!" << '\n';
+		}
+	}
+	return 0;
+}
