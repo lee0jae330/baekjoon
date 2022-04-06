731a08a7f5c33b60fa44021e9ea0df11c5900eba
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-06 20:48:58 +0900

    [Gold IV] Title: 플로이드, Time: 88 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+#define INF 123456789
+
+int dist[101][101];
+
+int main(void) {
+	int N,M;
+	cin >>N>>M;
+	for (int i = 0; i < M; i++) {
+		int a, b,c;
+		cin >>a>>b>>c;
+		if(!dist[a][b])
+			dist[a][b]=c;
+		else
+			dist[a][b]=min(dist[a][b],c);
+	}
+
+
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if(!dist[i][j]&&i!=j)
+				dist[i][j]=INF;
+		}
+	}
+
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+			}
+		}
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if(dist[i][j]==INF||i==j)
+				cout << 0 <<' ';
+			else
+				cout << dist[i][j]<<' ';
+		}
+		cout <<'\n';
+	}
+	return 0;
+}
