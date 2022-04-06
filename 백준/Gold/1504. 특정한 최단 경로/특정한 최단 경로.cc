b0cb0e334bea1e6d55a58559a3ea29c608d1f887
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-06 23:40:22 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 60 ms, Memory: 5744 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,12 +1,9 @@
 #include<iostream>
-#include<queue>
 #include<algorithm>
 #include<vector>
-#include<memory.h>
+#include<queue>
 
 using namespace std;
-
-#define INF 123456789
 
 void fastio() {
 	ios_base::sync_with_stdio(false);
@@ -14,28 +11,26 @@ void fastio() {
 	cout.tie(NULL);
 }
 
+#define INF 123456789
+
+int dist[801];
 vector<pair<int, int>>v[801];
 
-int distS[801];
-
-void dijk(int s) {
-
-	memset(distS, INF, sizeof(distS));
-
+void dijk(int x) {
 	priority_queue<pair<int, int>>pq;
-	pq.push({ 0,s });
-	distS[s] = 0;
+	dist[x] = 0;
+	pq.push({ 0,x });
 	while (!pq.empty()) {
 		int d = -pq.top().first;
-		int cur = pq.top().second;
+		x = pq.top().second;
 		pq.pop();
-		if (distS[cur] < d)
+		if (d > dist[x])
 			continue;
-		for (int i = 0; i < v[cur].size(); i++) {
-			int next = v[cur][i].first;
-			int cost = d + v[cur][i].second;
-			if (cost < distS[next]) {
-				distS[next] = cost;
+		for (int i = 0; i < v[x].size(); i++) {
+			int next = v[x][i].first;
+			int cost = v[x][i].second + d;
+			if (dist[next] > cost) {
+				dist[next] = cost;
 				pq.push({ -cost, next });
 			}
 		}
@@ -44,52 +39,85 @@ void dijk(int s) {
 
 int main(void) {
 	fastio();
-	int N, E;
-	cin >> N >> E;
-	for (int i = 0; i < E; i++) {
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
 		int a, b, cost;
 		cin >> a >> b >> cost;
 		v[a].push_back({ b,cost });
 		v[b].push_back({ a,cost });
 	}
-	int x, y;
-	cin >> x >> y;
-	
-	if (!E) {
-		cout << -1 << '\n';
-		return 0;
-	}
-
+	int v1, v2;
+	cin >> v1 >> v2;
+	fill(dist, dist + 801, INF);
 	dijk(1);
-	bool X=true, Y=true;
-	int distX = distS[x], distY=distS[y];
-	if (distX == INF)
-		X = false;
-	if (distY == INF)
-		Y = false;
-	dijk(x);
-	if (X) 
-		distX += distS[y];
-	if (Y)
-		distY += distS[y];
-
-	dijk(y);
-	if (X)
-		distX += distS[N];
-	dijk(x);
-	if (Y)
-		distY += distS[N];
-	if (X && Y) {
-		cout << min(distX, distY) << '\n';
+	int x = dist[v1], y = dist[v2];
+	bool V1 = true, V2 = true;
+	if (x == INF) {
+		V1=false;
 	}
-	else if (X) {
-		cout << distX << '\n';
+	if (y == INF) {
+		V2=false;
 	}
-	else if (Y)
-		cout << distY << '\n';
-	else
-		cout << -1 << '\n';
+	if (V1&&V2) {
+		fill(dist, dist+801,INF);
+		dijk(v1);
+		x+=dist[v2];
+		fill(dist, dist + 801, INF);
+		dijk(v2);
+		x+=dist[N];
 
+		fill(dist, dist + 801, INF);
+		dijk(v2);
+		y+=dist[v1];
+		fill(dist, dist + 801, INF);
+		dijk(v1);
+		y+=dist[N];
+		if(min(x,y)>=INF)
+			cout << -1<<'\n';
+		else
+			cout << min(x,y)<<'\n';
+
+	}
+	else if(V1){
+		fill(dist, dist + 801, INF);
+		dijk(v1);
+		if (dist[v2] == INF) {
+			cout << -1<<'\n';
+		}
+		else {
+			x+=dist[v2];
+			fill(dist, dist + 801, INF);
+			dijk(v2);
+			if (dist[N] == INF) {
+				cout<<-1<<'\n';
+			}
+			else{
+				x+=dist[N];
+				cout << x <<'\n';
+			}
+		}
+	}
+	else if(V2){
+		fill(dist, dist+801 ,INF);
+		dijk(v2);
+		if (dist[v1] == INF) {
+			cout <<-1<<'\n';
+		}
+		else {
+			y+=dist[v1];
+			fill(dist, dist + 801, INF);
+			dijk(v1);
+			if(dist[N]==INF)
+				cout << -1 <<'\n';
+			else {
+				y+=dist[N];
+				cout << y <<'\n';
+			}
+		}
+	}
+	else {
+		cout << -1<<'\n';
+	}
 	return 0;
-
 }
