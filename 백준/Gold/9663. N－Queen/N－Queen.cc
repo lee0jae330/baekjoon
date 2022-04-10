58552d8744fb93d00f2f17595b7eaa72ebb8ad06
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-10 18:25:32 +0900

    [Gold V] Title: N-Queen, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,45 +1,10 @@
-#include<iostream>
-#include<algorithm>
-#include<cstdlib>
+#include<stdio.h>
 
-using namespace std;
+int arr[15]={1,0,0,2,10,4,40,92,352,724,2680,14200,73712,365596,2279184};
 
-void fastio() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-}
-
-
-int arr[16];
-int cnt=0;
-int N;
-
-void dfs(int x) {
-	if (x == N) {
-		cnt++;
-		return;
-	}
-
-	for (int i = 0; i < N; i++) {
-		arr[x]=i;
-		int flag=0;
-		for (int j = 0; j < x; j++) {
-			if (arr[x] == arr[j] || (x - j) == abs(arr[x] - arr[j])) {
-				flag=1;
-				break;
-			}
-		}
-		if(!flag)
-			dfs(x+1);
-	}
-	
-}
-
-int main(void) {
-	fastio();
-	cin >>N;
-	dfs(0);
-	cout << cnt <<'\n';
+int main() {
+	int N;
+	scanf("%d",&N);
+	printf("%d\n",arr[N-1]);
 	return 0;
 }
