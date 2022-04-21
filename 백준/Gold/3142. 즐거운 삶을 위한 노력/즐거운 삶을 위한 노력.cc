07cd4a5a52258013edd702a44c16d2dd1033fd39
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-21 21:05:20 +0900

    [Gold II] Title: 즐거운 삶을 위한 노력, Time: 120 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/3142. 즐거운 삶을 위한 노력/즐거운 삶을 위한 노력.cc
+++ 백준/Gold/3142. 즐거운 삶을 위한 노력/즐거운 삶을 위한 노력.cc
@@ -1,9 +1,5 @@
 #include<iostream>
-#include<algorithm>
-#include<map>
 #include<cmath>
-#include<vector>
-#include<set>
 
 using namespace std;
 
@@ -13,11 +9,9 @@ void fastio() {
 	cout.tie(NULL);
 }
 
-map<int, int>m;
 int arr[1000001];
 int cnt[1000001];
-vector<int>prime;
-set<int>s;
+
 int main(void) {
 	fastio();
 	arr[1] = 1;
@@ -25,15 +19,14 @@ int main(void) {
 		if (arr[i])
 			continue;
 		arr[i] = i;
-		prime.push_back(i);
 		for (long long j = i * i; j < 1000001; j += i)
 			arr[j] = i;
 	}
-	int N=500000;
+	int N;
 	cin >> N;
 	long long sum = 0;
 	while (N--) {
-		int num=999999;
+		int num;
 		cin >> num;
 		while (num != 1) {
 			int tmp = arr[num];
@@ -47,25 +40,7 @@ int main(void) {
 			}
 			num /= arr[num];
 		}
-		/*while (1) {
-			for (int i = 0; i < prime.size(); i++) {
-				if (num % prime[i] == 0) {
-					if (cnt[prime[i]] % 2 == 0) {
-						sum++;
-						cnt[prime[i]]++;
-					}
-					else {
-						sum--;
-						cnt[prime[i]]++;
-					}
-					num /= prime[i];
-				}
-				if (num < prime[i])
-					break;
-			}
-			if (num == 1)
-				break;
-		}*/
+		
 		if (sum == 0)
 			cout << "DA" << '\n';
 		else
