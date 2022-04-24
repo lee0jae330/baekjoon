ceeedb6b952b02c19a8b7d9b44b4de53557609e2
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-24 17:55:10 +0900

    [Silver II] Title: 내 선물을 받아줘 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.cc
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.cc
@@ -1,8 +1,5 @@
 #include<iostream>
-#include<queue>
-#include<set>
-#include<vector>
-#include<memory.h>
+#include<string>
 
 using namespace std;
 
@@ -12,61 +9,18 @@ void fastio() {
 	cout.tie(NULL);
 }
 
-char arr[1001];
-int visited[1001];
-bool isOverlap;
-set<int> s;
-vector<int> v;
-void bfs(int x) {
-	queue<int>q;
-	visited[x] = 1;
-	q.push(x);
-	v.push_back(x);
-	while (!q.empty()) {
-		x = q.front();
-		q.pop();
-		if (arr[x] == 'E') {
-			if (!visited[x + 1]) {
-				visited[x + 1] = 1;
-				q.push(x + 1);
-				v.push_back(x + 1);
-			}
-		}
-		else {
-			if (!visited[x - 1]) {
-				visited[x - 1] = 1;
-				q.push(x - 1);
-				v.push_back(x - 1);
-			}
-		}
-	}
-}
-
 int main(void) {
+	fastio();
 	int N;
 	cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+	string str;
+	cin >> str;
 	int cnt = 0;
-	for (int i = 0; i < N; i++) {
-		isOverlap = false;
-		v.clear();
-		memset(visited, 0, sizeof(visited));
-		bfs(i);
-		int flag = 0;
-		for (int i = 0; i < v.size(); i++) {
-			if (s.find(v[i]) == s.end()) {
-				s.insert(v[i]);
-			}
-			else {
-				flag = 1;
-			}
-		}
-
-		if (!flag) {
+	for (int i = 0; i < str.length() - 1; i++) {
+		if (str[i] == 'E' && str[i + 1] == 'W') {
 			cnt++;
 		}
 	}
-	cout << cnt << '\n';
+	cout << cnt <<'\n';
 	return 0;
 }
