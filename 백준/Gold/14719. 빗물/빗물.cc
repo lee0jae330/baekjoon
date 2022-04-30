fa316beff2acf38439cd43b4e8e01af105b239ef
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-04-30 15:39:04 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,53 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int arr[501][501];
+
+int main(void) {
+	fastio();
+	int H, W;
+	cin >> H >> W;
+	for (int i = 0; i < W; i++) {
+		int num;
+		cin >> num;
+		int tmp = H-1;
+		for (int j = 0; j < num; j++) {
+			arr[tmp--][i] = 1;
+		}
+	}
+	/*for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++)
+			cout << arr[i][j] << ' ';
+		cout << '\n';
+	}*/
+	int result = 0;
+	int l=0;
+	for (int i = H - 1; i >= 0; i--) {
+		int	cnt = 0;
+		l = 0;
+		for (int j = 0; j < W; j++) {
+			if (arr[i][j]) {
+				if (!l) {
+					l = 1;
+				}
+				else {
+					result += cnt;
+					cnt = 0;
+				}
+			}
+			else {
+				if (l)
+					cnt++;
+			}
+		}
+	}
+	cout << result << '\n';
+	return 0;
+}
