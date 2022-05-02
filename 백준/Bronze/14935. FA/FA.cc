2907a6e9598547dc6b06b8390bea96d578164fee
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-02 15:01:39 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	string s;
+	cin >> s;
+	cout << "FA" << '\n';
+	return 0;
+}
