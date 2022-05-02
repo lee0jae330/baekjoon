0764ad6d3ab31d29a541c699ae2ed3a7e31fa6e1
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-02 22:21:07 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	int N;
+	cin >> N;
+	cout << pow(2,N) << '\n';
+	return 0;
+}
