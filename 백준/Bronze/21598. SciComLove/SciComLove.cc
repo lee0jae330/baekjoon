9d0e05fd38e024496510ba43cadada36198a7b73
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-02 16:53:01 +0900

    [Bronze IV] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	long long N;
+	cin >> N;
+	for (long long i = 0; i < N; i++)
+		cout << "SciComLove" << '\n';
+	return 0;
+}
