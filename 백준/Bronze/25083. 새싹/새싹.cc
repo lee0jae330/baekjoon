1db6d80699c2ed1392b4def3e6c80af62dc3dbf8
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-02 14:19:04 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	//fastio();
+	cout << "         ,r'\"7" << '\n';
+	cout << "r`-_   ,'  ,/" << '\n';
+	cout <<" \\. \". L_r'"<<'\n';
+	cout << "   `~\\/" << '\n';
+	cout << "      |" << '\n';
+	cout << "      |";
+	return 0;
+}
