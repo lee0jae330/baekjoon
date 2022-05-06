720e7e88a6fd7b9ee6e377ee6e38084d09ba74cf
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-06 16:26:16 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N * 5; j++)
+			cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N * 5; j++)
+			cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < N*2; i++) {
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		cout << '\n';
+	}
+	return 0;
+}
