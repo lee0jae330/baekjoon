8f905ec3f5f0355223edc22f2c8262b8760024e9
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-06 23:48:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	long long N;
+	cin >> N;
+	cout << N * N << '\n' << 2 << '\n';
+	return 0;
+}
