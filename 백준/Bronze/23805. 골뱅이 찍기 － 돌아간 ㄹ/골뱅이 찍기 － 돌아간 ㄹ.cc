a2f6cdd0a37e43fb604fe3a05cedd3fc11466578
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2022-05-06 17:30:30 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+void fastio() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main(void) {
+	fastio();
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N * 3; j++)
+			cout << '@';
+		for (int j = 0; j < N; j++)
+			cout << ' ';
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < N * 3; i++) {
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		for (int j = 0; j < N; j++)
+			cout << ' ';
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		for (int j = 0; j < N; j++)
+			cout << ' ';
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		cout << '\n';
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cout << '@';
+		for (int j = 0; j < N; j++)
+			cout << ' ';
+		for (int j = 0; j < N * 3; j++)
+			cout << '@';
+		cout << '\n';
+	}
+
+
+	return 0;
+}
