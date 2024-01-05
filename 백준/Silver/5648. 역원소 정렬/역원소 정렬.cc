a9875d9531a3b1844821df5b8a8cdf14d3629014
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2024-01-05 22:10:58 +0900

    [Silver V] Title: 역원소 정렬, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.cc
@@ -2,31 +2,7 @@
 
 using namespace std;
 
-vector<string>v;
-vector<long long>numbers;
-
-string rev(string& s) {
-	string tmp;
-	int flag = 0;
-	for (int i = s.length() - 1; i >= 0; i--) {
-		if (s[i] == '0') {
-			if (!flag) {
-				continue;
-			}
-			else {
-				tmp.push_back(s[i]);
-			}
-		}
-		else {
-			flag = 1;
-			tmp.push_back(s[i]);
-		}
-	}
-	if (tmp.empty()) {
-		tmp.push_back('0');
-	}
-	return tmp;
-}
+vector<long long> v;
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -36,15 +12,12 @@ int main(void) {
 	while (N--) {
 		string s;
 		cin >> s;
-		v.push_back(s);
+		reverse(s.begin(),s.end());
+		v.push_back(stoll(s));
 	}
-
-	for (string s : v) {
-		numbers.push_back(stoll(rev(s)));
-	}
-	sort(numbers.begin(),numbers.end());
-	for (long long num : numbers) {
-		cout << num <<'\n';
+	sort(v.begin(),v.end());
+	for (long long num : v) {
+		cout << num << '\n';
 	}
 	return 0;
 }
