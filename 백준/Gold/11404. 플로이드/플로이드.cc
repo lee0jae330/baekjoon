3b78157528f3fd5bdeba370144f4716dca1c4275
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2024-04-26 04:51:55 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,56 +1,51 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-void fastio() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-}
 
-#define INF 123456789
+const int INF = 1e9;
+int N,M;
 
-int dist[101][101];
+int arr[101][101];
 
-int main(void) {
-	fastio();
-	int N,M;
-	cin >>N>>M;
-	for (int i = 0; i < M; i++) {
-		int a, b,c;
-		cin >>a>>b>>c;
-		if(!dist[a][b])
-			dist[a][b]=c;
-		else
-			dist[a][b]=min(dist[a][b],c);
-	}
-
-
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if(!dist[i][j]&&i!=j)
-				dist[i][j]=INF;
-		}
-	}
-
+void floydWarshall() {
 	for (int k = 1; k <= N; k++) {
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
-				dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+				arr[i][j] = min(arr[i][j], arr[i][k]+arr[k][j]);
 			}
 		}
 	}
-	
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> N >> M;
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= N; j++) {
+			arr[i][j] =INF;
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a, b, cost;
+		cin >> a>> b >> cost;
+		arr[a][b] = min(arr[a][b],cost);
+	}
+
+	floydWarshall();
+
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			if(dist[i][j]==INF||i==j)
+			if (i == j || arr[i][j] == INF) {
 				cout << 0 <<' ';
+			}
 			else
-				cout << dist[i][j]<<' ';
+				cout << arr[i][j]<< ' ';
 		}
 		cout <<'\n';
 	}
 	return 0;
+
 }
