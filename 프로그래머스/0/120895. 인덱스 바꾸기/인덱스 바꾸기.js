794d4ff7f108a5e7985b2165dd6335ce2b616d0d
lee0jae330 <71895560+lee0jae330@users.noreply.github.com>
2024-07-06 18:58:14 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,6 @@
+function solution(my_string, num1, num2) {
+    const arr = my_string.split("");
+    [arr[num1], arr[num2]]  = [arr[num2], arr[num1]];
+    var answer = arr.join("");
+    return answer;
+}
