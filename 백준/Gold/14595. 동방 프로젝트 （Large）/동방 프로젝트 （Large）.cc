37fdbc13b433156d2c202a802b4f69bed0dd92d8
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2024-12-17 03:33:28 +0900

    [Gold IV] Title: 동방 프로젝트 (Large), Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/14595. 동방 프로젝트 （Large）/동방 프로젝트 （Large）.cc
+++ 백준/Gold/14595. 동방 프로젝트 （Large）/동방 프로젝트 （Large）.cc
@@ -8,58 +8,13 @@ using namespace std;
 
 const int SIZE = 1e6+1;
 
-int parent[SIZE];
-int ranking[SIZE];
 set<pair<int,int>>input;
-set<int>s;
-int arr[SIZE];
-
-void init() {
-	for (int i = 0; i < SIZE; i++) {
-		parent[i] = i;
-		ranking[i] = 0;
-	}
-}
-
-int find(int x) {
-	if (x == parent[x]) {
-		return x;
-	}
-	return parent[x] = find(parent[x]);
-}
-
-void merge(int x, int y) {
-	x = find(x);
-	for (int i = x + 1; i <= y; i++) {
-		int rootY = find(i);
-		if (x == rootY) {
-			return;
-		}
-		if (ranking[x] > ranking[y]) {
-			parent[rootY] = x;
-		}
-		else if (ranking[x] < ranking[y]) {
-			parent[x] = rootY;
-		}
-		else {
-			ranking[x]++;
-			parent[rootY] = x;
-		}
-		find(i);
-	}
-
-	
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-	init();
 	
-	for (int i = 0; i < SIZE; i++) {
-		arr[i] = i;
-	}
 	int N, M;
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
@@ -90,8 +45,6 @@ int main() {
 	}
 
 	sol -= (mx - mn );
-
-
 
 	cout << sol <<'\n';
 	
