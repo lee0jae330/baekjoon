6b1252dea24669c354c2f7b88c4e8cfbea2f668b
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2024-12-30 23:28:06 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 12 ms, Memory: 3564 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+
+vector<int>arr;
+vector<int> lis;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+
+	vector<int> tmp = arr;
+	sort(tmp.begin(), tmp.end());
+	if (tmp == arr) {
+		cout << 0 <<'\n';
+		return 0;
+	}
+
+	for (int i = 0; i < N; i++) {
+		auto iter = lower_bound(lis.begin(), lis.end(),arr[i]);
+		if (iter == lis.end()) {
+			lis.push_back(arr[i]);
+		}
+		else {
+			*iter = arr[i];
+		}
+	}
+
+	cout << N - lis.size() <<'\n';
+	return 0;
+}
