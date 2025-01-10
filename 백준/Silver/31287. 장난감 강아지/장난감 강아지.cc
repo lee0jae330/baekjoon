9644a3e3ba154e2f87ccb0a3681ee43f2f0acf3a
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-01-11 02:00:39 +0900

    [Silver II] Title: 장난감 강아지, Time: 352 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/31287. 장난감 강아지/장난감 강아지.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N, K;
+	cin >> N >> K;
+	string str;
+	cin >> str;
+
+	int x = 0, y = 0;
+	for (int i = 0; i < min((int)1e5, K); i++) {
+		for (char c : str) {
+			if (c == 'U') {
+				y++;
+			}
+			else if (c == 'D') {
+				y--;
+			}
+			else if (c == 'L') {
+				x--;
+			}
+			else {
+				x++;
+			}
+
+			if (!x && !y) {
+				cout << "YES" << '\n';
+				return 0;
+			}
+		}
+	}
+	cout << "NO" << '\n';
+	return 0;
+}
