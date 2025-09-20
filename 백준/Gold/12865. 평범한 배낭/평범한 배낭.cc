99f9679a182d9ca5ea502feaf91c56b0079018e6
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-09-21 02:45:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,31 +1,34 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
-int weight[101];
-int value[101];
-
 int dp[101][100001];
+pair<int,int>arr[101];
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	int N,K;
+	cout.tie(nullptr);
+
+	int N , K;
 	cin >> N >> K;
-	
+
 	for (int i = 1; i <= N; i++) {
-		cin >> weight[i] >> value[i];
+		cin >> arr[i].first >> arr[i].second;
 	}
-	
+
 	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= K; j++) {
+		for (int j = 0; j <= K; j++) {
 			dp[i][j] = dp[i-1][j];
-			if (j - weight[i] >= 0) {
-				dp[i][j] = max(dp[i][j],dp[i-1][j-weight[i]]+value[i]);
+			if (j - arr[i].first >= 0) {
+				dp[i][j] = max(dp[i][j], dp[i-1][j-arr[i].first]+ arr[i].second);
 			}
 		}
 	}
 
 	cout << dp[N][K] <<'\n';
 	return 0;
+ 
 }
