f184effcc998329e713d3839566f8a55995ce69f
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-09-21 05:00:53 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<utility>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int C, N;
+
+const int INF = 1 << 31 - 1;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> C >> N;
+
+	vector<int>dp(10001, INF);
+	vector<pair<int, int>>v(N);
+	dp[0] = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+
+	for (int i = 1; i <= 10000; i++) {
+		for (auto [cost, value] : v) {
+			if (i - value >= 0) {
+				dp[i] = min(dp[i], dp[i - value] + cost);
+			}
+		}
+	}
+
+	int sol = dp[C];
+	for (int i = C + 1; i <= 10000; i++) {
+		sol = min(sol, dp[i]);
+	}
+
+	cout << sol <<'\n';
+	return 0;
+
+}
