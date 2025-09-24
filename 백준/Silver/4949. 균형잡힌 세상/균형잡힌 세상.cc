08f843376f51766fe70942604509e814eceb7726
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-09-24 12:58:02 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,59 +1,55 @@
 #include<iostream>
 #include<string>
 #include<stack>
+
 using namespace std;
 
-void fastio() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-int main(void) {
-    while (1) {
-        string s1;
-        getline(cin, s1);
-        if (s1.size() == 1 && s1[0] == '.')
-            break;
-        stack<char>s;
-        int flag = 0;
-        for (int i = 0; i < s1.size(); i++) {
-            char tmp = s1[i];
-            if (tmp == '(' || tmp == '[') {
-                s.push(tmp);
-            }
-            else if (tmp == ')' || tmp == ']') {
-                char t;
-                if (!s.empty()) {
-                    t = s.top();
-                    if (tmp == ')') {
-                        if (t == '(') {
-                            s.pop();
-                        }
-                        else {
-                            flag = 1;
-                        }
-                    }
-                    else if (tmp == ']') {
-                        if (t == '[')
-                            s.pop();
-                        else {
-                            flag = 1;
-                        }
-                    }
-                }
-                else {
-                    flag = 1;
-                }
-            }
-            if (flag)
-                break;
-        }
-        if (s.empty()&&flag==0) {
-            cout << "yes" << '\n';
-        }
-        else
-            cout << "no" << '\n';
-    }
-   
-    return 0;
+	string str;
+	while (getline(cin, str)) {
+		if (str == ".") {
+			return 0;
+		}
+		stack<char>s;
+		int flag = 0;
+
+		for (char c : str) {
+			if (c == '(') {
+				s.push('(');
+			}
+			else if (c == '[') {
+				s.push('[');
+			}
+			else if (c == ')') {
+				if (!s.empty() &&s.top() == '(') {
+					s.pop();
+				}
+				else {
+					flag = 1;
+					break;
+				}
+			}
+			else if (c == ']') {
+				if (!s.empty() && s.top() == '[') {
+					s.pop();
+				}
+				else {
+					flag = 1;
+					break;
+				}
+			}
+		}
+
+		if (flag || !s.empty()) {
+			cout <<"no" <<'\n';
+		}
+		else {
+			cout << "yes" <<'\n';
+		}
+	}
+	return 0;
 }
