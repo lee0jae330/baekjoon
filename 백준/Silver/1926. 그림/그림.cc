bfe9610841c71ceadb8cd509d3c00b53dd2fd619
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-11-04 15:35:07 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,52 +1,43 @@
 #include<iostream>
 #include<queue>
-#include<utility>
 
-using namespace std;
-
+int N, M;
 int arr[501][501];
 int visited[501][501];
-
 int xx[4] = { 0,1,0,-1 };
 int yy[4] = { 1,0,-1,0 };
 
-int sol = 0;
+using namespace std;
 
-int N, M;
-
-
-void bfs(int x, int y) {
+int bfs(int x, int y) {
 	queue<pair<int, int>>q;
 	q.emplace(x, y);
 	visited[x][y] = 1;
 	int cnt = 0;
-
 	while (!q.empty()) {
-		auto [x, y] = q.front();
+		auto[x, y] = q.front();
 		q.pop();
 		cnt++;
-
 		for (int i = 0; i < 4; i++) {
 			int tx = x + xx[i];
 			int ty = y + yy[i];
-
-			if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty] || !arr[tx][ty]) {
+			if (tx < 0 || ty < 0 || tx >= N || ty >= M || visited[tx][ty]) {
 				continue;
 			}
 
-			q.emplace(tx, ty);
-			visited[tx][ty] = 1;
+			if (arr[tx][ty]) {
+				q.emplace(tx, ty);
+				visited[tx][ty] = 1;
+			}
 		}
 	}
-
-	sol = max(sol, cnt);
+	return cnt;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-
 
 	cin >> N >> M;
 
@@ -55,18 +46,17 @@ int main() {
 			cin >> arr[i][j];
 		}
 	}
-
+	int sol = 0;
 	int cnt = 0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			if (arr[i][j] && !visited[i][j]) {
-				bfs(i,j);
+				sol = max(sol, bfs(i, j));
 				cnt++;
 			}
 		}
 	}
-
-
-	cout << cnt <<'\n' << sol <<'\n';
+	cout << cnt << '\n';
+	cout << sol << '\n';
 	return 0;
 }
