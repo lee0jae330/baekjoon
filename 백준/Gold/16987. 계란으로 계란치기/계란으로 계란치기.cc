73c24dff225b28873e1cf6750965c31fd72c8490
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2025-11-11 17:54:15 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -3,50 +3,65 @@
 using namespace std;
 
 int N;
-vector<pair<int,int>>egg;
-int broken;
-int c;
-void rec(int idx, int cnt){ //인덱스, 깬 갯수
-    if(idx==N){
-        broken=max(broken,cnt);
-        return;
-    }
-    if(egg[idx].first<=0){
-        rec(idx+1,cnt);
-        return;
-    }
-    int tmp=0,f=1;
-    for(int i=0;i<N;i++){ //1.가장왼쪽계란,2.계란을 들고 한번 치면 해당 계란을 내려 놓음
-        if(i==idx)
-            continue;
-        if(egg[i].first>0){
-            f=0;
-            egg[idx].first -=egg[i].second;
-            egg[i].first -=egg[idx].second;
-            if(egg[idx].first<=0)
-                tmp++;
-            if(egg[i].first<=0)
-                tmp++;
-            rec(idx+1,cnt+tmp);
-            egg[idx].first += egg[i].second;
-            egg[i].first +=egg[idx].second;
-            tmp=0;
-        }
-    }
-    if(f)
-        rec(idx+1,cnt);
+vector<pair<int,int>>v;
+
+int sol = 0;
+
+void backtrack(int idx, int cnt) {
+	if (idx == N) {
+		sol = max(sol, cnt);
+		return;
+	}
+
+	if (v[idx].first <= 0) {
+		backtrack(idx+1, cnt);
+		return;
+	}
+
+	int flag =0;
+	for (int i = 0; i < N; i++) {
+
+		if (i == idx) {
+			continue;
+		}
+
+		if (v[i].first > 0) {
+			flag =1;
+			int tmp = cnt;
+			v[i].first -= v[idx].second;
+			v[idx].first -= v[i].second;
+			if (v[i].first <= 0) {
+				tmp++;
+			}
+			if (v[idx].first <= 0) {
+				tmp++;
+			}
+
+			backtrack(idx+1, tmp);
+		
+			v[i].first += v[idx].second;
+			v[idx].first += v[i].second;
+		}
+	}
+	if (!flag) {
+		backtrack(idx+1, cnt);
+	}
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin >> N;
-    for(int i=0;i<N;i++){
-        int s,w;
-        cin >> s>>w;
-        egg.push_back({s,w});
-    }
-    rec(0,0);
-    cout << broken;
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N;
+	v.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+
+	backtrack(0,0);
+
+	cout << sol <<'\n';
+	return 0;
 }
